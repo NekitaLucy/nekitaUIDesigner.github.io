579e8dd1b83a97ea645c9d6e27e8c8045c15f0a1
NekitaLucy <nekitalucy2015@gmail.com>
2023-07-26 13:29:42 -0400

    commit
    push changes

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+
+// alert ("Bonjour, Hello. Welcome to my UI Website Portfolio!");
+
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+
+
